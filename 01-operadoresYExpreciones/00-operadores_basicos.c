8e31c4ed56a6c81dcafb7885e5831cd93bd7be79
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-21 12:01:38 -0300

    Operadores basicos en C

---
+++ 01-operadoresYExpreciones/00-operadores_basicos.c
@@ -0,0 +1,41 @@
+//OPERADORES ARITMETICOS
+
+/*
+OPERADOR        NOMBRE        SIGNIFICADO        
+   +            Aditivo       Suma
+   -            Inverso       Resta
+   *            Producto      Multiplicacion
+   /            Cociente      Divicion
+   %            Modulo        Residuo de una divicion
+   ++           Incremento    Suma 1
+   --           Decremento    Resta 1
+*/
+
+//OPERADORES RELACIONALES
+/*
+OPERADOR        SIGNIFICADO
+   <            Menor que
+   >            Mayor que
+   <=           Menor o igual
+   >=           Mayor o igual
+   ==           Igual a
+*/
+
+//OPERADORES LOGICOS
+/*
+OPERADOR       NOMBRE        SIGNIFICADO
+   !           NOT           Negacion 
+   &&          AND           Insercion(Y)
+   ||          OR            Union(O) 
+*/
+
+//OPERADORES DE ASIGNACION
+/*
+OPERADOR       SIGNIFICADO
+   =           Asignacion
+   +=          Suma y asignacion
+   -=          Resta y asignacion
+   *=          Multiplicacion y asignacion
+   /=          Divicion y asignacion
+   %=          Modulo y asignacion
+*/
